680aaf239addea7d7ab0a51fe3d42ea1ff307675
akshay <97163087+akshayexists@users.noreply.github.com>
2025-09-12 07:41:10 +0800

    Update main.py
    more logging
    
    Signed-off-by: akshay <97163087+akshayexists@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,14 +2,15 @@ from runsim import runsim
 from exportcleaner import exportcleaner
 
 #look at all available teams
-teams = None    #change to list of teams if specific teams are to be tested eg. ['GSW', 'BOS', 'NOL']
+teams = []    #change to list of teams if specific teams are to be tested eg. ['GSW', 'BOS', 'NOL']
 
 seed = 69
 print("Master seed:", seed)
-runs = 2000
+runs = 500
 path = 'output.csv'
 data = exportcleaner(export_file='data/export.json', teaminfo_file='data/teaminfo.json', teams = teams)
-print(data)
+data.to_csv('inputs.csv')
+#print(data)
 
 sim = runsim(seed=seed)
 df = sim.PROGEMUP(data, runs=runs)
