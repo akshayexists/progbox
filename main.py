b188bf978ba4df3cf41b05a1f813fcf4eb398c2b
akshay <97163087+akshayexists@users.noreply.github.com>
2025-09-11 13:58:45 +0800

    Update main.py
    Signed-off-by: akshay <97163087+akshayexists@users.noreply.github.com>

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from exportcleaner import exportcleaner
 #look at all available teams
 teams = None    #change to list of teams if specific teams are to be tested eg. ['GSW', 'BOS', 'NOL']
 
-seed = 69420
+seed = 69
 print("Master seed:", seed)
-runs = 200
-
+runs = 2000
+path = 'output.csv'
 data = exportcleaner(export_file='data/export.json', teaminfo_file='data/teaminfo.json', teams = teams)
 print(data)
 
 sim = runsim(seed=seed)
 df = sim.PROGEMUP(data, runs=runs)
-df.to_csv("montecarlo.csv", index=False)
+df.to_csv(path)
+#df.to_excel(path, sheet_name='monte carlo', index=False, engine="xlsxwriter")
+print(f'Written to {path}')
